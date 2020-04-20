561cab8d53c8bc541d9feba59a130a046b7fd11b
Olzhas Kurikov <olzhas2102@yahoo.com>
2020-04-20 22:39:48 +0200

    currying

---
+++ currying.js
@@ -0,0 +1,7 @@
+const add = (x) => (y) => x + y
+
+const addThree = add(3)
+
+console.log(addThree(4))
+console.log(addThree(12))
+console.log(addThree(34))
--- immutable-data-structure.js
+++ immutable-data-structure.js
@@ -18,5 +18,48 @@ const push = (value) => (array) => {
 	return clone
 }
 
-const a = [1, 2, 3]
-const b = push(4)(a)
+const c = [1, 2, 3]
+const d = push(4)(c)
+console.log(c) // [1, 2, 3]
+console.log(d) // [1, 2, 3, 4]
+
+// taking a drink from a glass
+
+class MutableGlass {
+	constructor(content, amount) {
+		this.content = content
+		this.amount = amount
+	}
+
+	takeDrink(value) {
+		this.amount = Math.max(
+			this.amount - value,
+			0
+		) // to make sure as a result we have positive number
+		return this
+	}
+}
+
+const mg1 = new MutableGlass('water', 100)
+const mg2 = mg1.takeDrink(20)
+console.log(mg1)
+console.log(mg2)
+
+class ImmutableGlass {
+	constructor(content, amount) {
+		this.content = content
+		this.amount = amount
+	}
+
+	takeDrink(value) {
+		return new ImmutableGlass(
+			this.content,
+			Math.max(this.amount - value, 0)
+		)
+	}
+}
+
+const ig1 = new ImmutableGlass('water', 100)
+const ig2 = ig1.takeDrink(20)
+console.log(ig1)
+console.log(ig2)

006f5ee1e308071d561fef7b18a4743fa6c8c560
Olzhas Kurikov <olzhas2102@yahoo.com>
2020-04-21 12:20:55 +0200

    pointfree

---
+++ pointfree.js
@@ -0,0 +1,9 @@
+// pointfree programming
+
+const array = [2, 4, 7]
+const double = (item) => item * 2
+console.log(array.map(double))
+
+// legibility
+// reduce surface area for bugs
+// unit test our named functions
